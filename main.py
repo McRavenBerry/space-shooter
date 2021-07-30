3ae40808adbccee380073d5feabe5c8726af6dad
Trevor McGarrah <86972467+McRavenBerry@users.noreply.github.com>
2021-07-30 10:17:01 -0500

    Full game with life, high score, splash screen

--- main.py
+++ main.py
@@ -1,6 +1,75 @@
+def collision(sprite, otherSprite):
+    sprite.destroy(effects.fire, 100)
+    otherSprite.destroy(effects.fire, 100)
+    scene.camera_shake(20, 1000)
+    music.big_crash.play()
+    pause(1000)
+    info.player1.change_life_by(-1)
+
+def hit(sprite, otherSprite):
+    sprite.destroy()
+    otherSprite.destroy(effects.fire, 100)
+    scene.camera_shake(5, 100)
+    music.big_crash.play()
+    info.player1.change_score_by(1)
+
+def fire():
+    laser = sprites.create_projectile_from_sprite(img("""
+        . . . . . . 5 5 5 . . . . . . .
+        . . . . . 5 5 1 5 5 . . . . . .
+        . . . . . 5 1 1 1 5 . . . . . .
+        . . . . . 5 1 1 1 5 . . . . . .
+        . . . . . . 5 1 5 . . . . . . .
+        . . . . . . 5 5 5 . . . . . . .
+        . . . . . . . 5 . . . . . . . .
+        . . . . . . . 5 . . . . . . . .
+        . . . . . . . 5 . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+    """), spaceship, 0, -50)
+    laser.set_kind(SpriteKind.projectile)
+    music.pew_pew.play()
+    laser.set_flag(SpriteFlag.AUTO_DESTROY, True)
+
+
+def spawner():
+    yVel = randint(20, 50)
+    asteroid = sprites.create_projectile_from_sprite(img("""
+        . . . . . . . c c c a c . . . .
+        . . c c b b b a c a a a c . . .
+        . c c a b a c b a a a b c c . .
+        . c a b c f f f b a b b b a . .
+        . c a c f f f 8 a b b b b b a .
+        . c a 8 f f 8 c a b b b b b a .
+        c c c a c c c c a b c f a b c c
+        c c a a a c c c a c f f c b b a
+        c c a b 6 a c c a f f c c b b a
+        c a b c 8 6 c c a a a b b c b c
+        c a c f f a c c a f a c c c b .
+        c a 8 f c c b a f f c b c c c .
+        . c b c c c c b f c a b b a c .
+        . . a b b b b b b b b b b b c .
+        . . . c c c c b b b b b c c . .
+        . . . . . . . . c b b c . . . .
+    """), None, 0, yVel)
+
+    xPos = randint(0, scene.screen_width())
+    asteroid.set_position(xPos, 0)
+    asteroid.set_kind(SpriteKind.enemy)
+    asteroid.set_flag(SpriteFlag.AUTO_DESTROY, True)
 
 #Background
+game.splash("Battlefield Galactica")
 effects.star_field.start_screen_effect()
+
+#Set Score and Life 
+info.player1.set_life(1)
+info.player1.set_score(0)
 
 #Spaceship
 spaceship = sprites.create(img("""
@@ -25,3 +94,13 @@ spaceship.set_position(75, 111)
 spaceship.set_kind(SpriteKind.player)
 controller.move_sprite(spaceship, 100, 0)
 spaceship.set_stay_in_screen(True)
+
+#Spawn asteroids
+game.on_update_interval(1000, spawner)
+
+#Set up the fire button
+controller.A.on_event(ControllerButtonEvent.PRESSED, fire)
+
+#Check for collisions
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, collision)
+sprites.on_overlap(SpriteKind.enemy, SpriteKind.projectile, hit)
--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 // Background
+game.splash("Battlefield Galactica")
 effects.starField.startScreenEffect()
+// Set Score and Life 
+info.player1.setLife(1)
+info.player1.setScore(0)
 // Spaceship
 let spaceship = sprites.create(img`
     . . . . . . . c d . . . . . . .
@@ -23,3 +27,69 @@ spaceship.setPosition(75, 111)
 spaceship.setKind(SpriteKind.Player)
 controller.moveSprite(spaceship, 100, 0)
 spaceship.setStayInScreen(true)
+// Spawn asteroids
+game.onUpdateInterval(1000, function spawner() {
+    let yVel = randint(20, 50)
+    let asteroid = sprites.createProjectileFromSprite(img`
+        . . . . . . . c c c a c . . . .
+        . . c c b b b a c a a a c . . .
+        . c c a b a c b a a a b c c . .
+        . c a b c f f f b a b b b a . .
+        . c a c f f f 8 a b b b b b a .
+        . c a 8 f f 8 c a b b b b b a .
+        c c c a c c c c a b c f a b c c
+        c c a a a c c c a c f f c b b a
+        c c a b 6 a c c a f f c c b b a
+        c a b c 8 6 c c a a a b b c b c
+        c a c f f a c c a f a c c c b .
+        c a 8 f c c b a f f c b c c c .
+        . c b c c c c b f c a b b a c .
+        . . a b b b b b b b b b b b c .
+        . . . c c c c b b b b b c c . .
+        . . . . . . . . c b b c . . . .
+    `, null, 0, yVel)
+    let xPos = randint(0, scene.screenWidth())
+    asteroid.setPosition(xPos, 0)
+    asteroid.setKind(SpriteKind.Enemy)
+    asteroid.setFlag(SpriteFlag.AutoDestroy, true)
+})
+// Set up the fire button
+controller.A.onEvent(ControllerButtonEvent.Pressed, function fire() {
+    let laser = sprites.createProjectileFromSprite(img`
+        . . . . . . 5 5 5 . . . . . . .
+        . . . . . 5 5 1 5 5 . . . . . .
+        . . . . . 5 1 1 1 5 . . . . . .
+        . . . . . 5 1 1 1 5 . . . . . .
+        . . . . . . 5 1 5 . . . . . . .
+        . . . . . . 5 5 5 . . . . . . .
+        . . . . . . . 5 . . . . . . . .
+        . . . . . . . 5 . . . . . . . .
+        . . . . . . . 5 . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+    `, spaceship, 0, -50)
+    laser.setKind(SpriteKind.Projectile)
+    music.pewPew.play()
+    laser.setFlag(SpriteFlag.AutoDestroy, true)
+})
+// Check for collisions
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function collision(sprite: Sprite, otherSprite: Sprite) {
+    sprite.destroy(effects.fire, 100)
+    otherSprite.destroy(effects.fire, 100)
+    scene.cameraShake(20, 1000)
+    music.bigCrash.play()
+    pause(1000)
+    info.player1.changeLifeBy(-1)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function hit(sprite: Sprite, otherSprite: Sprite) {
+    sprite.destroy()
+    otherSprite.destroy(effects.fire, 100)
+    scene.cameraShake(5, 100)
+    music.bigCrash.play()
+    info.player1.changeScoreBy(1)
+})
